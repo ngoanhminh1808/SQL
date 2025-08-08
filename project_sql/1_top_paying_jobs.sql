71c8422451e25586fe8fdbf046957c8b42e0d8b9
ngoanhminh1808 <ngoanhminh180808@gmail.com>
2025-08-08 22:06:33 +0700

    top paying jobs

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,13 @@
+SELECT
+    job_title,
+    job_location,
+    salary_year_avg
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst'
+    AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY    
+    salary_year_avg DESC
+LIMIT 5
