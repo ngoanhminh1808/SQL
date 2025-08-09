48b9c22859323a067cb60670c1970993760ec74d
ngoanhminh1808 <ngoanhminh180808@gmail.com>
2025-08-09 09:01:20 +0700

    Top paying Data Analyst jobs skills

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,9 +1,12 @@
 SELECT
     job_title,
     job_location,
+    cd.name,
+    job_schedule_type,
     salary_year_avg
 FROM
-    job_postings_fact
+    job_postings_fact AS jp
+LEFT JOIN company_dim as cd ON cd.company_id = jp.company_id
 WHERE
     job_title_short = 'Data Analyst'
     AND job_location = 'Anywhere'
